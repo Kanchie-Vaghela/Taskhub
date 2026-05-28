41c6189b8d0163e76813446c50d5ba2159e8e8db
kanchi <KanchiVaghela.ict23@adaniuni.ac.in>
2026-05-29 00:39:09 +0530

    feat: add google oauth and github

--- frontend/app/(public)/login/page.tsx
+++ frontend/app/(public)/login/page.tsx
@@ -1,7 +1,37 @@
-const login = () => {
-  return (
-    <div>login</div>
-  )
-}
+"use client";
 
-export default login
+import { supabase } from "@/lib/supabase";
+
+const LoginPage = () => {
+  const handleGoogleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+
+  const handleGithubLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+      <button
+        onClick={handleGoogleLogin}
+        className="border px-4 py-2 rounded"
+      >
+        Continue with Google
+      </button>
+
+      <button
+        onClick={handleGithubLogin}
+        className="border px-4 py-2 rounded"
+      >
+        Continue with GitHub
+      </button>
+    </div>
+  );
+};
+
+export default LoginPage;
